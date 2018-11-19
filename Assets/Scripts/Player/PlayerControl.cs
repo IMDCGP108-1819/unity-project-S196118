c435d5078e8372be635d86ac975d97a40c1787f3
Amesileo <gheathcollins@gmail.com>
2018-11-19 15:34:09 +0000

    Fixing iss
    Fixed menu issue with start button - would not load
    Begin to work on Player controller - set up left side binding and begin on coding

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    // Player Movement
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime;
+
+    }
+    // Screen Binding - Left-hand side
+    private void LateUpdate()
+    {
+        float currentX = transform.position.x;
+
+        if ( currentX < -8.3f)
+        {
+            transform.position = new Vector3(-8.3f, transform.position.y);
+        }
+    }
+}
+
+	
