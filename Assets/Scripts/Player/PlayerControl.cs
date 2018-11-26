967235ae3c796f6cd15fdf158ae313d2bbafb5c9
alex-turnbull <32357150+alex-turnbull@users.noreply.github.com>
2018-11-26 15:01:31 +0000

    Jump control refinment
    refined jump control and movement speed to get the effect of low gravity

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -4,18 +4,26 @@ public class PlayerControl : MonoBehaviour {
     private int score;
 
     // jump control
-    public float JumpCheckPosition;
-    public float JumpCheckRange = 1.0f;
+    public Rigidbody2D rb;
+    public float JumpForce = 5.0f;
 
-    public float MaxSpeed;
+    public float MaxSpeed = 5.0f;
 
     // Player Movement
     void Update()
     {   
         float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
-        float vertical = Input.GetAxis("Vertical") * Time.deltaTime;
+        //float vertical = Input.GetAxis("Vertical") * Time.deltaTime;
 
-        transform.position = new Vector3(transform.position.x + horizontal * MaxSpeed, transform.position.y + vertical * MaxSpeed);
+        transform.position = new Vector3(transform.position.x + horizontal * MaxSpeed, transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0.0f, JumpForce), ForceMode2D.Impulse);
+        }
     }
 
     // Screen Binding - Left-hand side
