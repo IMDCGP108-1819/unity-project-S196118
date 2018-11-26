692a8e77b874cfe7eeb1526c682b92f1ccb8a60c
alex-turnbull <32357150+alex-turnbull@users.noreply.github.com>
2018-11-26 14:33:01 +0000

    movment
    Got controls to work by adding transform.position function.
    Jump constraints added

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
-{
+public class PlayerControl : MonoBehaviour {
+    private int score;
+
+    // jump control
+    public float JumpCheckPosition;
+    public float JumpCheckRange = 1.0f;
+
+    public float MaxSpeed;
 
     // Player Movement
     void Update()
-    {
+    {   
         float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
         float vertical = Input.GetAxis("Vertical") * Time.deltaTime;
 
+        transform.position = new Vector3(transform.position.x + horizontal * MaxSpeed, transform.position.y + vertical * MaxSpeed);
     }
+
     // Screen Binding - Left-hand side
     private void LateUpdate()
     {
@@ -22,4 +30,4 @@ public class PlayerControl : MonoBehaviour
     }
 }
 
-	
+ 
