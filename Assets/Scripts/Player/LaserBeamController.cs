c3e8e14496cc088915788b3f2f082832607d5866
S196118 <s196118@uos.ac.uk>
2019-01-04 19:46:56 +0000

    Shooting Update
    The Laser now shoots in correct direction and with correct velocity

---
+++ Assets/Scripts/Player/LaserBeamController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamController : MonoBehaviour
+{
+
+    public float speed = 40f;
+    public Rigidbody2D rb;
+
+    // Laser Velocity
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}
