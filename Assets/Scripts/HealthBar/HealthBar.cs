923aca67a47df82d1069977b52c55750ea59a00c
S196118 <s196118@uos.ac.uk>
2019-01-08 17:05:15 +0000

    Created Health script
    Created Health script
    Following youtubt tutorial
    https://www.youtube.com/watch?v=l11fkFoFfrg

---
+++ Assets/Scripts/HealthBar/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    Image healthBar;
+    float MaxHealth = 100f;
+    public static float Health;
+
+	// Use this for initialization
+	void Start () {
+        healthBar = GetComponent<Image>();
+        Health = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthBar.fillAmount = Health / MaxHealth;
+	}
+}
