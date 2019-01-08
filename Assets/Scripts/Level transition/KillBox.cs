092123a009d827c3c7a56d8821c448210b6e7e6c
S196118 <s196118@uos.ac.uk>
2019-01-08 15:52:27 +0000

    Fixed Killbox
    set killbox to reload level when player collides with the box collider

--- Assets/Scripts/Level transition/KillBox.cs
+++ Assets/Scripts/Level transition/KillBox.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class KillBox : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Level1");
+    }
 }
+	
