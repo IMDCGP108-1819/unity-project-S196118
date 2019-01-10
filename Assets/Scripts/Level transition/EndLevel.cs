41ac514b18f395474b3057d2d4dcc7964d8c9f65
S196118 <s196118@uos.ac.uk>
2019-01-10 16:47:02 +0000

    level transition
    created level transition script
    and began work on level 2

--- Assets/Scripts/Level transition/EndLevel.cs
+++ Assets/Scripts/Level transition/EndLevel.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Upon collision load level 2
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
