4b6a4f4911253c5725e8066b07364fa52c747789
S196118 <s196118@uos.ac.uk>
2019-01-10 13:25:33 +0000

    Updated health pickup script
    health pick up script works as intended

--- Assets/Scripts/Health Pickup/HealthPickUp.cs
+++ Assets/Scripts/Health Pickup/HealthPickUp.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPickUp : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class HealthPickUp : MonoBehaviour
+{
+    // If the player collides with the player then player gains health
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag.Equals("Player"))
+        {
+            HealthBar.Health += 10f;
+        }
+        Destroy(gameObject);
+    }
 }
+
+   
+
